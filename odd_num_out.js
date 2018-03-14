0f04c5251b1dd62e0a76d3c835f9e8005f91def6
cgroark <colingroark@gmail.com>
2018-03-14 15:11:58 -0700

    num out problem added

---
+++ odd_num_out.js
@@ -0,0 +1,10 @@
+//find the one number in an array that doesn't match the others
+
+function stray(numbers) {
+  numbers.sort()
+  if(numbers[0] !== numbers[1]){
+    return numbers[0]
+  }else return numbers[numbers.length-1]
+}
+
+stray([27, 27, 1, 27, 27, 27, 27])
